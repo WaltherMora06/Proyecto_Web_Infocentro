4bfecf6f495b3d68c820270af2dcf51a54b3bde3
WaltherMora06 <86491402+WaltherMora06@users.noreply.github.com>
2021-11-19 11:27:02 -0500

    Create login_usuario.php

---
+++ login_usuario.php
@@ -0,0 +1,26 @@
+<link rel="stylesheet" href="CSS/loging.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<div class="main">
+    <div class="container">
+        <center>
+            <div class="middle">
+                <div id="login">
+                    <form action="login.php" method="POST">
+                        <fieldset class="clearfix">
+                        <input name="nombre" type="text" placeholder="Ingrese Sus Nombres">
+                        <input name="apellido" type="text" placeholder="Ingrese Sus Apellidos">
+                        <input name="cedula" type="text" placeholder="Ingrese Su Cedula ">
+                        <input type="submit" value="Ingresar">
+                        </fieldset>
+                        <div class="clearfix"></div>
+                    </form>
+                    <div class="clearfix"></div>
+                </div> <!-- Fin del Loging -->
+                <div class="logo">
+                    <img src="./Imagenes/logo_puntos_grande.png">
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </center>
+    </div>
+</div>
